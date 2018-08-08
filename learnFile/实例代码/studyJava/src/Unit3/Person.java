c5b434a2cbd96230485b47aa2c7e5270031ceaa5
JHZ <jinhaizhong@lenovohit.com>
2018-08-08 09:48:26 +0800

    修改person

--- learnFile/实例代码/studyJava/src/Unit3/Person.java
+++ learnFile/实例代码/studyJava/src/Unit3/Person.java
@@ -1,6 +1,6 @@
 package Unit3;
 
-class Person {
+class Person implements Comparable<Person> {
 
 	private String name;
 	private Integer age;
@@ -8,10 +8,41 @@ class Person {
 	public Person(String name, Integer age) {
 		this.age = age;
 		this.name = name;
+
 	}
 
 	@Override
-	public String toString() {
-		return "ÐÕÃû:" + name;
+	public boolean equals(Object obj) {
+		// if (obj.getClass() != this.getClass()) {
+		// return false;
+		// } else {
+		// Person p = (Person) obj;
+		// return p.getName().equals(this.name) && p.getAge().equals(this.age);
+		// }
+
+		Person p = (Person) obj;
+		return p.getName().equals(this.name) && p.getAge().equals(this.age);
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public int compareTo(Person p) {
+		return p.age - age;
+	}
+
 }
